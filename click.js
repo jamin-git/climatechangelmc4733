f6fc26b15e4508235064c552d024f8f63b2b76bd
jamin <63824095+jamin-git@users.noreply.github.com>
2023-04-16 23:48:03 -0400

    directions, rotation, camera-look at

--- click.js
+++ click.js
@@ -1,3 +1,24 @@
+// This will Pop Up the Instructions Pages
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   Swal.fire({
+//     imageUrl: 'resources/directions/pagecombined.png',
+//     imageAlt: 'Welcome Image',
+//     width: "100%",
+//     backdrop: "rgba(1, 1, 1, .99)"
+//   });
+// });
+
+function showDirections() {
+  Swal.fire({
+    imageUrl: 'resources/directions/pagecombined.png',
+    imageAlt: 'Welcome Image',
+    width: "100%",
+    backdrop: "rgba(1, 1, 1, .99)"
+  });
+}
+
+
 // This will toggle the visibility of the Markers
 
 AFRAME.registerComponent('marker1', {
@@ -24,7 +45,7 @@ AFRAME.registerComponent('marker1', {
     },
     init: function () {
       this.el.addEventListener('click', function () {
-        var x = document.getElementById("marker2text");
+        var x = document.getElementById("coral2");
 
         if (x.getAttribute("visible") == true) {
             x.setAttribute("visible", false);
